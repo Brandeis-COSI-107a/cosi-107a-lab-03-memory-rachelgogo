61fb97c8e8b02895aa9fd1ebfba5950019ca3a43
rachel <ruixuegou@brandeis.edu>
2025-02-14 14:32:55 -0500

    update

--- print-size.c
+++ print-size.c
@@ -7,16 +7,16 @@ int main() {
     char *char_ptr;
 
     // Print sizes of various types
-    printf("The size of char is %zu\n", sizeof(char));
-    printf("The size of short is %zu\n", sizeof(short));
-    printf("The size of int is %zu\n", sizeof(int));
-    printf("The size of long is %zu\n", sizeof(long));
-    printf("The size of long long is %zu\n", sizeof(long long));
-    printf("The size of float is %zu\n", sizeof(float));
-    printf("The size of double is %zu\n", sizeof(double));
-    printf("The size of char * is %zu\n", sizeof(char *));
-    printf("The size of int * is %zu\n", sizeof(int *));
-    printf("The size of long * is %zu\n", sizeof(long *));
+    printf("size of char is %zu\n", sizeof(char));
+    printf("size of short is %zu\n", sizeof(short));
+    printf("size of int is %zu\n", sizeof(int));
+    printf("size of long is %zu\n", sizeof(long));
+    printf("size of long long is %zu\n", sizeof(long long));
+    printf("size of float is %zu\n", sizeof(float));
+    printf("size of double is %zu\n", sizeof(double));
+    printf("size of char * is %zu\n", sizeof(char *));
+    printf("size of int * is %zu\n", sizeof(int *));
+    printf("size of long * is %zu\n", sizeof(long *));
 
     // Print the size of the character array
     printf("The size of my character array is %zu\n", sizeof(char_array));
